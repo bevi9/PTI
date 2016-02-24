0f9a9625b72e6aab075c1b8ae0f5fcbe309577e9
bevi9 <bervia9@gmail.com>
2016-02-24 10:13:15 -0500

    First commit

---
+++ pti_1/list.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+# Llibreries
+import cgi, os, re, sys, string, time, csv
+
+# Programa principal
+print "Content-type: text/html\n\n"
+c = csv.reader(open("car.csv","rb"))
+for row in c:
+	print row
+
+

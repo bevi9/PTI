45cbc137cfa32af021479189f20d72fdfe9ad9be
bevi9 <bervia9@gmail.com>
2016-02-28 22:56:26 +0100

    commit

--- pti_1/list.py
+++ pti_1/list.py
@@ -5,8 +5,8 @@ import cgi, os, re, sys, string, time, csv
 
 # Programa principal
 print "Content-type: text/html\n\n"
-c = csv.reader(open("car.csv","rb"))
+
+c = csv.reader(open('car.csv', 'rb'))
 for row in c:
 	print ', '.join(row)
-
 
--- pti_1/new.py
+++ pti_1/new.py
@@ -7,20 +7,23 @@ import cgi, os, re, sys, string, time, csv
 # Programa principal
 print "Content-type: text/html\n\n"
 
-home = "http://localhost/p1/carrental_home.html"
-new = "http://localhost/p1/carrental_form_new.html"
-
-c = csv.writer(open("car.csv","a+"))
 form = cgi.FieldStorage()
 model_vehicle = form.getvalue('model_vehicle')
 if model_vehicle == "54":
-	 model_vehicle = "Econòmic"
+	model_vehicle = "Economic"
+	cost = 10 
 elif model_vehicle == "71": 
 	model_vehicle = "Semi-luxe"
+	cost = 20
 elif model_vehicle == "82":
-	 model_vehicle = "Luxe"
+	model_vehicle = "Luxe"
+	cost = 30
 elif model_vehicle == "139":
-	 model_vehicle = "Limusina"
+	model_vehicle = "Limusina"
+	cost = 40
+
+home = "http://localhost/p1/carrental_home.html"
+new = "http://localhost/p1/carrental_form_new.html"
 
 engine = form.getvalue('sub_model_vehicle')
 try:
@@ -54,15 +57,21 @@ if (days <= 0 or des <= 0 or units <= 0 or (des).is_integer() or des > 1) and ok
     ok = 1
   	
 else:
+    if des == 0.0: cost = cost * days * units
+    elif des == 1.0: cost = 0
+    else: cost = cost * des * days * units
     print '<html>'
     print  '<body>'
-    print ' <h1>Has introduit el cotxe perfectament</h1>'
+    print ' <h2>Has introduit el cotxe perfectament</h2>'
+    print ' <h2>Cost del servei % s $</h2>' % cost
+    print ' <h2>Per llogar % s cotxe/s % s i % s durant % s dia/es amb el descompte 
+    print '		del % s</h2' % (units, model_vehicle, engine, days, des)
     print '   <head>'
-    print '     <meta http-equiv="refresh" content="3;url=%s" />' % home
-    print "     <h3>Redirigint a la pagina d'inici</h3>"
+    print '     <a href="%s">Home</a>' % home
     print '   </head>'
     print ' </body>'
     print '</html>'
-    c.writerow([model_vehicle,engine,days,units,des])
+    c = csv.writer(open("car.csv","a"))
+    c.writerow([model_vehicle, engine, days, units, des, cost])
 
 

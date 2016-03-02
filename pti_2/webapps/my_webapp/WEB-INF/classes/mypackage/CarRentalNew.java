826a8a9dc93301b735cd736f9296c9d454a8bd77
bevi9 <bervia9@gmail.com>
2016-03-02 19:25:07 +0100

    Fet el new, falta la correccio derrors

--- pti_2/webapps/my_webapp/WEB-INF/classes/mypackage/CarRentalNew.java
+++ pti_2/webapps/my_webapp/WEB-INF/classes/mypackage/CarRentalNew.java
@@ -12,17 +12,27 @@ public class CarRentalNew extends HttpServlet {
                     throws ServletException, IOException {
     res.setContentType("text/html");
     PrintWriter out = res.getWriter();
+	
+
+	File f = new File("/var/lib/tomcat7/webapps/my_webapp/WEB-INF/classes/mypackage/car.txt");
+
     String model = req.getParameter("model_vehicle");
     String engine = req.getParameter("sub_model_vehicle");
-	String num = req.getParameter("num_vehicle");
+ 	String num = req.getParameter("num_vehicle");
+	String dies = req.getParameter("dies_lloguer");
 	String descompte = req.getParameter("descompte"); 
     cont ++;
-	if(model == "54") model = "economic";
-	else if(model == "71") model = "semi-luxe";
-	else if(model == "82") model = "luxe";
-	else if(model == "139") model = "limusina";
-    out.println("<html><big>Hola Amigo\n Tu modelo = "+ model + "\nTu engine = "+ engine + "</big><br>"+  
-                cont + " Accesos desde su carga.</html>");
+	if(model.equals("54")) model = "Econòmic";
+	else if(model.equals("71")) model = "Semi-luxe";
+	else if(model.equals("82")) model = "Luxe";
+	else if(model.equals("139")) model = "Limusina";
+
+	PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("/var/lib/tomcat7/webapps/my_webapp/WEB-INF/classes/mypackage/car.txt", true)));
+	writer.println(model + ", " + engine + ", " + num + ", " + dies + ", " + descompte);
+
+	writer.close();
+
+    out.println("<html><big>Hola Amigo\n Tu modelo = "+ model + "\nTu engine = "+ engine + "</big><br>"+ cont + " Accesos desde su carga.</html>");
   }
 
   public void doPost(HttpServletRequest req, HttpServletResponse res)
